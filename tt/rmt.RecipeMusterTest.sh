85042629dee68b26f2831f7f58d8b22feb8be2c8
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-09-14 11:17:10 -0700

    Get help.

--- tt/rmt.RecipeMusterTest.sh
+++ tt/rmt.RecipeMusterTest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd "$(dirname "$0")/.." && Tools/tabtarget-dispatch.sh 1 "$(basename "$0")"
+cd "$(dirname "$0")/.." && echo "CURRENT DIR IS `pwd` and base is `basename $0`" && Tools/tabtarget-dispatch.sh 1 "$(basename "$0")"
