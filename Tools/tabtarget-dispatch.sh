df67d8566324e3787ff444bd6db3b818df7343e0
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-09-18 07:28:23 -0700

    I do believe Ive made progress.

--- Tools/tabtarget-dispatch.sh
+++ Tools/tabtarget-dispatch.sh
@@ -3,12 +3,6 @@
 
 set -euo pipefail
 
-# Path preparation
-SCRIPT_PATH="/usr/local/bin:/usr/bin:/bin:/cygdrive/c/Program Files/RedHat/Podman"
-
-# Ensure HOME is set and exported
-export HOME="${HOME:-/home/$(whoami)}"
-
 # First parameter is the number of jobs we'll let MAKE use. 1 is default non-parallel
 JOBS=$1
 shift
@@ -26,12 +20,19 @@ if [ "$JOBS" == "1" ]; then
     OUTPUT_SYNC=-Oline
 fi
 
-# Preserve the TERM environment variable for pretty colors
-CURRENT_TERM="${TERM:-xterm-256color}"
-
 # Start Podman machine if it's not already running
 podman machine start || echo "Podman probably running already, let's go on..."
 
-# Run make in a clean environment, but include TERM and HOME
-env -i HOME="$HOME" PATH="$SCRIPT_PATH" TERM="$CURRENT_TERM" \
-    make -f rmc-console.mk $OUTPUT_SYNC -j $JOBS $EXE $ARGS
+# Run make in a clean environment with specific variables pulled from current environment
+env -i \
+HOME="${HOME}" \
+USER="${USER}" \
+USERPROFILE="${USERPROFILE}" \
+PATH="${PATH}" \
+APPDATA="${APPDATA}" \
+TEMP="${TEMP}" \
+TMP="${TMP}" \
+TERM="${TERM}" \
+PODMAN_REMOTE=1 \
+PODMAN_USERNS=keep-id \
+make -f rmc-console.mk "$OUTPUT_SYNC" -j "$JOBS" "$EXE" $ARGS
