c996eafe3f7df79138e8aaeaf336391b739c9888
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-09-18 17:12:31 -0700

    Revived the srjcl build.

--- tt/mbsr-B__BuildImages.srjcl.sh
+++ tt/mbsr-B__BuildImages.srjcl.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-tools/elbm_ExecuteLoggingBashMake.sh 1 $(basename $0) MBSR_ARG_MONIKER=srjcl
+#!/bin/sh
+cd "$(dirname "$0")/.." &&  Tools/tabtarget-dispatch.sh 1 "$(basename "$0")" MBSR_ARG_MONIKER=srjcl
