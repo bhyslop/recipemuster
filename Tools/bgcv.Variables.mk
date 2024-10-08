b810c0220456dba8aa718d9b99bc3e7ee3b57701
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-08 07:52:59 -0700

    Okay, checker done.

--- Tools/bgcv.Variables.mk
+++ Tools/bgcv.Variables.mk
@@ -1,28 +1,30 @@
 include bgc-config.mk
 
-
 # Dynamic list of all BGCV_ variables
 BGCV_VARS := $(filter BGCV_%,$(.VARIABLES))
 
 # Specific list of required BGCV_ variables
-REQUIRED_BGCV_VARS := BGCV_REGISTRY_OWNER BGCV_REGISTRY_NAME BGCV_BUILD_ARCHITECTURES \
-                      BGCV_HISTORY_DIR BGCV_RECIPES_DIR BGCV_RECIPE_PATTERN \
-                      BGCV_TIMEOUT_MINUTES BGCV_CONCURRENCY BGCV_MAX_PARALLEL \
-                      BGCV_CONTINUE_ON_ERROR BGCV_FAIL_FAST
+REQUIRED_BGCV_VARS :=      \
+  BGCV_BUILD_ARCHITECTURES \
+  BGCV_CONCURRENCY         \
+  BGCV_CONTINUE_ON_ERROR   \
+  BGCV_FAIL_FAST           \
+  BGCV_HISTORY_DIR         \
+  BGCV_MAX_PARALLEL        \
+  BGCV_RECIPES_DIR         \
+  BGCV_RECIPE_PATTERN      \
+  BGCV_REGISTRY_NAME       \
+  BGCV_REGISTRY_OWNER      \
+  BGCV_TIMEOUT_MINUTES     \
 
 
 bgcv_check_rule:
 	@echo "Checking required variables..."
 	@for var in $(REQUIRED_BGCV_VARS); do \
-		if [ -z "$($$var)" ]; then \
-			echo "Error: Undefined required variable $$var"; \
-			exit 1; \
-		fi; \
+	  test -n "$${!var}" || (echo "Error: Undefined required variable $$var" && exit 1); \
 	done
 	@echo "All required variables are defined."
 
 
 bgcv_display_rule:
 	@$(foreach var,$(BGCV_VARS), echo "$(var)=$($(var))";)
-
-
--- bgc-config.mk
+++ bgc-config.mk
@@ -1,13 +1,13 @@
 
-BGCV_REGISTRY_OWNER      := bhyslop
-BGCV_REGISTRY_NAME       := recipemuster
-BGCV_BUILD_ARCHITECTURES := x86_64
-BGCV_HISTORY_DIR         := RBM-history
-BGCV_RECIPES_DIR         := RBM-recipes
-BGCV_RECIPE_PATTERN      := *.dockerfile
-BGCV_TIMEOUT_MINUTES     := 60
-BGCV_CONCURRENCY         := 2
-BGCV_MAX_PARALLEL        := 2
-BGCV_CONTINUE_ON_ERROR   := false
-BGCV_FAIL_FAST           := false
+export BGCV_REGISTRY_OWNER      := bhyslop
+export BGCV_REGISTRY_NAME       := recipemuster
+export BGCV_BUILD_ARCHITECTURES := x86_64
+export BGCV_HISTORY_DIR         := RBM-history
+export BGCV_RECIPES_DIR         := RBM-recipes
+export BGCV_RECIPE_PATTERN      := *.dockerfile
+export BGCV_TIMEOUT_MINUTES     := 60
+export BGCV_CONCURRENCY         := 2
+export BGCV_MAX_PARALLEL        := 2
+export BGCV_CONTINUE_ON_ERROR   := false
+export BGCV_FAIL_FAST           := false
 
