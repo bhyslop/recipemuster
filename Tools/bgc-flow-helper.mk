372ca9d6ba8aa8ca73f159e95352dffeb331266d
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-08 18:40:06 -0700

    fix-flow-helper

--- Tools/bgc-flow-helper.mk
+++ Tools/bgc-flow-helper.mk
@@ -6,16 +6,11 @@ BGCV_VARS := $(filter BGCV_%,$(.VARIABLES))
 # Specific list of required BGCV_ variables
 REQUIRED_BGCV_VARS :=      \
   BGCV_BUILD_ARCHITECTURES \
-  BGCV_CONCURRENCY         \
-  BGCV_CONTINUE_ON_ERROR   \
-  BGCV_FAIL_FAST           \
   BGCV_HISTORY_DIR         \
-  BGCV_MAX_PARALLEL        \
   BGCV_RECIPES_DIR         \
   BGCV_RECIPE_PATTERN      \
   BGCV_REGISTRY_NAME       \
   BGCV_REGISTRY_OWNER      \
-  BGCV_TIMEOUT_MINUTES     \
 
 
 bgcfh_check_rule:
--- bgc-config.mk
+++ bgc-config.mk
@@ -5,10 +5,5 @@ export BGCV_BUILD_ARCHITECTURES := x86_64
 export BGCV_HISTORY_DIR         := RBM-history
 export BGCV_RECIPES_DIR         := RBM-recipes
 export BGCV_RECIPE_PATTERN      := *.dockerfile
-export BGCV_TIMEOUT_MINUTES     := 60
-export BGCV_CONCURRENCY         := 2
-export BGCV_MAX_PARALLEL        := 2
-export BGCV_CONTINUE_ON_ERROR   := false
-export BGCV_FAIL_FAST           := false
 export BGCV_TOOLS_DIR           := Tools
 
