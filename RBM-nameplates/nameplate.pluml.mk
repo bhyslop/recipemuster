4dfb8ea8a1762e0ef0f510d18a877d5cdff51d43
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-19 09:04:01 -0700

    two more rigs, for the next.

---
+++ RBM-nameplates/nameplate.pluml.mk
@@ -0,0 +1,38 @@
+# © 2024 Scale Invariant.  All rights reserved.
+
+# Python Github Maintenance
+
+RBN_MONIKER = pluml
+
+RBN_SENTRY_REPO_FULL_NAME = ghcr.io/bhyslop/recipemuster
+RBN_ROGUE_REPO_FULL_NAME  = ghcr.io/bhyslop/recipemuster
+
+RBN_SENTRY_IMAGE_TAG = sentry_alpine.20241019__155818
+RBN_ROGUE_IMAGE_TAG  = bottle_plantuml.20241019__153313
+
+# NOT GOOD YET
+RBN_IP_HACK = 10.242
+
+# If the nameplate exports services through an application port, specify
+RBN_PORT_ENABLED = 0
+RBN_PORT_HOST    = 8000
+RBN_PORT_GUARDED = 8000
+
+# If the nameplate is allowed to reach out to the internet, specify
+# OUCH CHANGE THIS!!!  Expedient to try and viz stuff now
+RBN_OUTREACH_ENABLED = 0
+
+# Pretty unsafe thing, but needed for bootstrap
+RBN_WIDEREACH_ENABLED = 1
+RBN_WIDEREACH_DOMAIN  = github.com
+
+# Volume mounts and app directories:
+# Hmm, do I want a la carte or whole thing?
+RBN_APP_OUTER_DIR = ./RBM-environments-${RBN_MONIKER}
+RBN_APP_INNER_DIR = /mnt/rogue-data
+
+# If an autostart command, specify
+RBN_AUTOURL_ENABLED = 0
+
+
+# eof
