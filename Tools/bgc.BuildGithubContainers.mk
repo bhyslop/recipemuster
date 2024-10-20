41de71e5ab191ad322cb3debdaf5ad764609c6b3
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-20 08:34:52 -0700

    Draft.

--- Tools/bgc.BuildGithubContainers.mk
+++ Tools/bgc.BuildGithubContainers.mk
@@ -123,7 +123,7 @@ bgc-di%: zbgc_argcheck_rule
 	@test -s $(zBGC_VERSION_ID_CACHE)  ||\
 	  ($(MBC_SEE_RED) "Error: No version found for tag $(BGC_ARG_TAG)" && rm false)
 	@echo "Found version ID:" $(zBGC_VERSION_ID_CONTENTS) "for tag $(BGC_ARG_TAG)"
-	@echo $(MBC_SEE_YELLOW) && read -p "Confirm delete image? Type YES: " confirm && test "$$confirm" = "YES"  ||\
+	@echo $(MBC_SEE_YELLOW) "Confirm delete image?" && read -p "Type YES: " confirm && test "$$confirm" = "YES"  ||\
 	  ($(MBC_SEE_RED) "WONT DELETE" && false)
 	$(MBC_STEP) "Sending delete request..."
 	@curl -X DELETE $(zBGC_CURL_HEADERS) \
