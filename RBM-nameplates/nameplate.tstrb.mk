b5b098745436429f8d9ccc41513384b15abfc01a
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-21 06:17:08 -0700

    update nameplate

--- RBM-nameplates/nameplate.tstrb.mk
+++ RBM-nameplates/nameplate.tstrb.mk
@@ -7,7 +7,7 @@ RBN_MONIKER = tstrb
 RBN_SENTRY_REPO_FULL_NAME = ghcr.io/bhyslop/recipemuster
 RBN_BOTTLE_REPO_FULL_NAME = ghcr.io/bhyslop/recipemuster
 
-RBN_SENTRY_IMAGE_TAG = sentry_alpine.20241019__171511
+RBN_SENTRY_IMAGE_TAG = sentry_alpine.20241020__171441
 RBN_BOTTLE_IMAGE_TAG = bottle_python_networking.20241021__131414
 
 # NOT GOOD YET
