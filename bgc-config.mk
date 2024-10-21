a04110a3ffe9dbf3d8ab337dc7be98766fe10dec
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-21 07:15:32 -0700

    Lets try alternate platform string...

--- bgc-config.mk
+++ bgc-config.mk
@@ -1,7 +1,7 @@
 # Needed generally
 export BGCV_REGISTRY_OWNER      := bhyslop
 export BGCV_REGISTRY_NAME       := recipemuster
-export BGCV_BUILD_ARCHITECTURES := x86_64
+export BGCV_BUILD_ARCHITECTURES := linux/amd64
 export BGCV_HISTORY_DIR         := RBM-history
 export BGCV_RECIPES_DIR         := RBM-recipes
 export BGCV_TOOLS_DIR           := Tools
