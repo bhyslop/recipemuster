3f62dbb65936e75ab355eff4d896f318ba7d8e04
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-10-21 07:49:31 -0700

    Lock in latest sentry debian that has dnsmasq.

--- RBM-nameplates/nameplate.tstrb.mk
+++ RBM-nameplates/nameplate.tstrb.mk
@@ -7,7 +7,7 @@ RBN_MONIKER = tstrb
 RBN_SENTRY_REPO_FULL_NAME = ghcr.io/bhyslop/recipemuster
 RBN_BOTTLE_REPO_FULL_NAME = ghcr.io/bhyslop/recipemuster
 
-RBN_SENTRY_IMAGE_TAG = sentry_debian.20241021__143838
+RBN_SENTRY_IMAGE_TAG = sentry_debian.20241021__144818
 RBN_BOTTLE_IMAGE_TAG = bottle_python_networking.20241021__144537
 
 # NOT GOOD YET
