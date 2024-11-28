950aa2984c1c95d1bafd5eeea3a417e626b487c4
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-11-28 08:14:33 -0800

    Working on mbc and test now.

--- Tools/mbc.MakefileBashConsole.mk
+++ Tools/mbc.MakefileBashConsole.mk
@@ -10,13 +10,6 @@
 # Set below variable to add a localization context to pretty lines
 MBC_ARG__CONTEXT_STRING ?= mbu-c
 
-# Parameters extracted from tabtarget text delimited by '.'
-MBC_TTPARAM__FIRST  = $(word 1, $(subst ., ,$@))
-MBC_TTPARAM__SECOND = $(word 2, $(subst ., ,$@))
-MBC_TTPARAM__THIRD  = $(word 3, $(subst ., ,$@))
-MBC_TTPARAM__FOURTH = $(word 4, $(subst ., ,$@))
-MBC_TTPARAM__FIFTH  = $(word 5, $(subst ., ,$@))
-
 # No quotes since value is integer, not a string
 MBC_CONSOLEPARAM__COLS  := $(shell tput cols)
 MBC_CONSOLEPARAM__LINES := $(shell tput lines)
@@ -44,49 +37,6 @@ MBC_FAIL  := (printf $(zMBC_TPUT_RESET)$(zMBC_TPUT_RED)$(MBC_ARG__CONTEXT_STRING
 # For use in compound statements.
 MBC_SEE_RED    := printf '%s'$(MBC_ARG__CONTEXT_STRING)': %s %s %s %s %s %s %s %s %s\n'$(zMBC_TPUT_RESET) $(zMBC_TPUT_RED)
 MBC_SEE_YELLOW := printf '%s'$(MBC_ARG__CONTEXT_STRING)': %s %s %s %s %s %s %s %s %s\n'$(zMBC_TPUT_RESET) $(zMBC_TPUT_YELLOW)
-
-
-#################################
-# EXECUTION ENVIRONMENT CUSTOMIZATION
-# 
-zMBC_EEC_UNAME_S := $(shell uname -s)
-
-zMBC_EEC_TYPE_MAC      := 0
-zMBC_EEC_TYPE_LINUX    := 0
-zMBC_EEC_TYPE_CYGWIN   := 0
-zMBC_EEC_LABEL_TYPE    := __bad_label__
-ifneq   (,$(findstring CYGWIN_NT-10.0, $(zMBC_EEC_UNAME_S)))
-  zMBC_EEC_TYPE_CYGWIN := 1
-  zMBC_EEC_LABEL_TYPE  := cygwin_x86
-else ifeq   ($(zMBC_EEC_UNAME_S),Darwin)
-  zMBC_EEC_TYPE_MAC    := 1
-  zMBC_EEC_LABEL_TYPE  := mac_m1
-else ifeq   ($(zMBC_EEC_UNAME_S),Linux)
-  zMBC_EEC_TYPE_LINUX  := 1
-  zMBC_EEC_LABEL_TYPE  := linux_x86
-endif
-
-# On everything but MAC, the below pattern involving expanding a variable to select
-#   another variable that is then expanded works:
-MBC_ROOT_ABSDIR               := __unknown_execution_operating_system__
-MBC_ROOT_ABSDIR.cygwin_x86    := $(shell cygpath -ma .)
-MBC_ROOT_ABSDIR.linux_x86     := $(shell pwd)
-MBC_ROOT_ABSDIR.mac_m1        := $(shell pwd)
-MBC_ROOT_ABSDIR.__bad_label__ := __error_station_file_must_specify_operating_system__
-MBC_ROOT_ABSDIR               := $(MBC_ROOT_ABSDIR.$(zMBC_EEC_LABEL_TYPE))
-
-# If MACs are needed, prefer the following.  Needs testing
-# ITCH_EphemDockerScrub: switch to below because make complains under linux of no cygpath app
-ifeq      ($(zMBC_EEC_TYPE_CYGWIN),1)
-MBC_ALTROOT_ABSDIR  = $(MBC_ROOT_ABSDIR.cygwin_x86)
-else ifeq ($(zMBC_EEC_TYPE_MAC),1)
-MBC_ALTROOT_ABSDIR  = $(MBC_ROOT_ABSDIR.mac_m1)
-else ifeq ($(zMBC_EEC_TYPE_LINUX),1)
-MBC_ALTROOT_ABSDIR  = $(MBC_ROOT_ABSDIR.linux_x86)
-else 
-MBC_ALTROOT_ABSDIR := __unknown_execution_operating_system__
-endif
-
 
 
 
