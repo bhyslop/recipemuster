e0c8b87962a4cde639fdb061a349bc419f106cf0
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-12-08 07:30:02 -0800

    dumper dewarrnn..

--- Tools/rbm.RecipeBottleMakefile.mk
+++ Tools/rbm.RecipeBottleMakefile.mk
@@ -201,8 +201,11 @@ rbm-d%:
 
 
 rbm-T%:
-	@echo "Moniker:"$(RBM_ARG_MONIKER) "Explicit dnsmasq run"
-	podman exec $(RBM_SENTRY_CONTAINER) tcpdump -i any -n -vvv '(port 53) or (host $(RBB_DNS_SERVER) and port 53)'
+	@echo "Moniker:"$(RBM_ARG_MONIKER) "TCPDUMPER"
+	@echo "First, lets get process info so we know the dnsmasq is up..."
+	podman exec $(RBM_SENTRY_CONTAINER) ps aux
+	@echo "Now, lets tcpdump..."
+	podman exec $(RBM_SENTRY_CONTAINER) tcpdump -i eth0 -i eth1 -n -vvv '(port 53) or (host $(RBB_DNS_SERVER) and port 53)'
 
 
 # eof
