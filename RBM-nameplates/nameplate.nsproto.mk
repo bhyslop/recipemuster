90588b56e5de1f78ce8e54e9a566967e24b40452
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-01-14 08:18:13 -0800

    Enable connection to nsproto BOTTLE and SENTRY; no obvious reason networking fails.

--- RBM-nameplates/nameplate.nsproto.mk
+++ RBM-nameplates/nameplate.nsproto.mk
@@ -24,6 +24,12 @@ export RBN_UPLINK_ACCESS_GLOBAL   := 1
 export RBN_UPLINK_ALLOWED_CIDRS   := 160.79.104.0/23
 export RBN_UPLINK_ALLOWED_DOMAINS := anthropic.com
 
+# Network Configuration
+export RBN_ENCLAVE_BASE_IP    := 10.242.0.0
+export RBN_ENCLAVE_NETMASK    := 24
+export RBN_ENCLAVE_INITIAL_IP := 10.242.0.3
+export RBN_ENCLAVE_SENTRY_IP  := 10.242.0.2
+
 # Volume Mount Configuration
 export RBN_VOLUME_MOUNTS :=
 
