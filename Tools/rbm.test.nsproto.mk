c1446ce0fe4c6ecd7a44331ce378e192c7905497
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-01-23 07:02:31 -0800

    next

--- Tools/rbm.test.nsproto.mk
+++ Tools/rbm.test.nsproto.mk
@@ -19,14 +19,14 @@ rbm-t.TestRBM.nsproto.mk:
 
     
 	@echo "Get anthropic.com IP from sentry then try from bottle:"
-	@ANTHROPIC_IP=$$(podman exec $(TRBM_SENTRY) dig +short anthropic.com | head -1)  &&\
-	  echo "Testing allowed IP $$ANTHROPIC_IP"                                       &&\
-	  podman exec $(TRBM_BOTTLE) ping -c 2 $$ANTHROPIC_IP
+	@ANTHROPIC_IP=$$(podman exec $(RBM_SENTRY_CONTAINER) dig +short anthropic.com | head -1)  &&\
+	  echo "Testing allowed IP $$ANTHROPIC_IP"                                                &&\
+	  podman exec $(RBM_BOTTLE_CONTAINER) ping -c 2 $$ANTHROPIC_IP
 
 	@echo "Get google.com IP from sentry then try from bottle:"
-	@GOOGLE_IP=$$(podman exec $(TRBM_SENTRY) dig +short google.com | head -1)  &&\
-	  echo "Testing blocked IP $$GOOGLE_IP"                                    &&\
-	  ! podman exec $(TRBM_BOTTLE) ping -c 2 $$GOOGLE_IP
+	@GOOGLE_IP=$$(podman exec $(RBM_SENTRY_CONTAINER) dig +short google.com | head -1)  &&\
+	  echo "Testing blocked IP $$GOOGLE_IP"                                             &&\
+	  ! podman exec $(RBM_BOTTLE_CONTAINER) ping -c 2 $$GOOGLE_IP
 
 	@echo "PASS"
 
