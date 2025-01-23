3c80cafa0ecd480acba0b7fb0e68a788bb2c26ca
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-01-23 06:24:14 -0800

    failfast

--- Tools/rbm.test.nsproto.mk
+++ Tools/rbm.test.nsproto.mk
@@ -5,9 +5,9 @@ TRBM_BOTTLE = nsproto-bottle
 rbm-t.TestRBM.nsproto.mk:
 	@echo "Show that we can access internet from sentry"
 	podman exec -it $(TRBM_SENTRY) nslookup example.com
-	@echo "Show that we cannot access google from bottle"
-	! podman exec -it $(TRBM_BOTTLE) nslookup google.com
 	@echo "Show that we can access anthropic from bottle"
 	podman exec -it $(TRBM_BOTTLE) nslookup anthropic.com
+	@echo "Show that we cannot access google from bottle"
+	! podman exec -it $(TRBM_BOTTLE) nslookup google.com
 	@echo "PASS"
 
