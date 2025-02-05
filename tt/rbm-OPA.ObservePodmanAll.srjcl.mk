0de4a154d803836016df81c15886004f25e3c7b6
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-05 06:18:59 -0800

    Okay, now back finally at the tcpdumper...

--- tt/rbm-OPA.ObservePodmanAll.srjcl.mk
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")/.." &&  Tools/tabtarget-dispatch.sh 8 "$(basename "$0")"
---
+++ tt/rbm-OPA.ObservePodmanAll.srjcl.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd "$(dirname "$0")/.." &&  Tools/mbds_dispatch.sh jp_full om_line "$(basename "$0")"
+
