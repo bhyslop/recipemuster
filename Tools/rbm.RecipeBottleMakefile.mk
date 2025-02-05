d0d9ae3188375bc41a9f21419553717ced525b42
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-05 06:55:07 -0800

    pseudo-tty

--- Tools/rbm.RecipeBottleMakefile.mk
+++ Tools/rbm.RecipeBottleMakefile.mk
@@ -267,23 +267,15 @@ zRBM_TCPDUMP_BASE = -U -l -nn -vvv "$(zRBM_OPA_FILTER)"
 
 rbm-OPA-bottle:
 	podman machine ssh "echo === bottle ==="
-	podman machine ssh "which tcpdump"
-	podman machine ssh "sudo -n true && echo 'sudo ok' || echo 'sudo failed'"
-	podman machine ssh "ip netns list"
-	podman machine ssh "sudo -n journalctl -n 20" 2>&1
-	podman machine ssh "sudo -n dmesg | tail" 2>&1
-	podman machine ssh "sudo -n tcpdump -c 1 -i eth0" 2>&1
-	podman machine ssh "sudo -n journalctl -n 20" 2>&1
-	podman machine ssh "sudo -n dmesg | tail" 2>&1
-	podman machine ssh "sudo -n ip netns exec $(RBM_ENCLAVE_NAMESPACE) tcpdump -i eth0 $(zRBM_TCPDUMP_BASE) | cat" 2>&1
+	podman machine ssh -t "sudo -n ip netns exec $(RBM_ENCLAVE_NAMESPACE) tcpdump -i eth0 $(zRBM_TCPDUMP_BASE) | cat" 2>&1
 
 rbm-OPA-bridge:
 	podman machine ssh "echo === bridge ==="
-	podman machine ssh "sudo -n tcpdump -i $(RBM_ENCLAVE_BRIDGE) $(zRBM_TCPDUMP_BASE) | cat" 2>&1
+	podman machine ssh -t "sudo -n tcpdump -i $(RBM_ENCLAVE_BRIDGE) $(zRBM_TCPDUMP_BASE) | cat" 2>&1
 
 rbm-OPA-veth:
 	podman machine ssh "echo === veth ==="
-	podman machine ssh "sudo -n tcpdump -i $(RBM_ENCLAVE_BOTTLE_OUT) $(zRBM_TCPDUMP_BASE) | cat" 2>&1
+	podman machine ssh -t "sudo -n tcpdump -i $(RBM_ENCLAVE_BOTTLE_OUT) $(zRBM_TCPDUMP_BASE) | cat" 2>&1
 
 rbm-OPA-sentry:
 	podman machine ssh "echo === sentry ==="
