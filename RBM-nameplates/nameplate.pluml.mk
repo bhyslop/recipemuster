fcbfecb33b7ddb5d8e4f052ba6b7ace5a0f0d23d
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-06 05:42:06 -0800

    Big experiment begin: does plantuml work?

--- RBM-nameplates/nameplate.pluml.mk
+++ RBM-nameplates/nameplate.pluml.mk
@@ -1,38 +1,37 @@
 # © 2024 Scale Invariant.  All rights reserved.
 
-# Python Github Maintenance
+# Core Service Definition
+export RBN_MONIKER     := pluml
+export RBN_DESCRIPTION := Local PlantUML Server
 
-RBN_MONIKER = pluml
+# Image Source Configuration
+export RBN_SENTRY_REPO_PATH := ghcr.io/bhyslop/recipemuster
+export RBN_BOTTLE_REPO_PATH := ghcr.io/bhyslop/recipemuster
+export RBN_SENTRY_IMAGE_TAG := sentry_ubuntu_large.20250130__154657
+export RBN_BOTTLE_IMAGE_TAG := bottle_plantuml.20250206__133847
 
-RBN_SENTRY_REPO_PATH = ghcr.io/bhyslop/recipemuster
-RBN_BOTTLE_REPO_PATH = ghcr.io/bhyslop/recipemuster
+# Port Service Configuration
+export RBN_PORT_ENABLED           := 1
+export RBN_ENTRY_PORT_WORKSTATION := 8001
+export RBN_ENTRY_PORT_ENCLAVE     := 8080
 
-RBN_SENTRY_IMAGE_TAG = sentry_alpine.20241019__155818
-RBN_BOTTLE_IMAGE_TAG = bottle_plantuml.20241019__153313
+# Network Uplink Configuration
+export RBN_UPLINK_PORT_MIN        := 10000
+export RBN_UPLINK_DNS_ENABLED     := 0
+export RBN_UPLINK_ACCESS_ENABLED  := 0
+export RBN_UPLINK_DNS_GLOBAL      := 0
+export RBN_UPLINK_ACCESS_GLOBAL   := 0
+export RBN_UPLINK_ALLOWED_CIDRS   :=
+export RBN_UPLINK_ALLOWED_DOMAINS :=
 
-# NOT GOOD YET
-RBN_IP_HACK = 10.242
+# Network Configuration
+export RBN_ENCLAVE_BASE_IP    := 10.242.0.0
+export RBN_ENCLAVE_NETMASK    := 24
+export RBN_ENCLAVE_SENTRY_IP  := 10.242.0.2
+export RBN_ENCLAVE_BOTTLE_IP  := 10.242.0.3
 
-# If the nameplate exports services through an application port, specify
-RBN_PORT_ENABLED = 0
-RBN_PORT_HOST    = 8000
-RBN_PORT_GUARDED = 8000
-
-# If the nameplate is allowed to reach out to the internet, specify
-# OUCH CHANGE THIS!!!  Expedient to try and viz stuff now
-RBN_OUTREACH_ENABLED = 0
-
-# Pretty unsafe thing, but needed for bootstrap
-RBN_WIDEREACH_ENABLED = 1
-RBN_WIDEREACH_DOMAIN  = github.com
-
-# Volume mounts and app directories:
-# Hmm, do I want a la carte or whole thing?
-RBN_APP_OUTER_DIR = ./RBM-environments-${RBN_MONIKER}
-RBN_APP_INNER_DIR = /mnt/bottle-data
-
-# If an autostart command, specify
-RBN_AUTOURL_ENABLED = 0
+# Volume Mount Configuration
+export RBN_VOLUME_MOUNTS := 
 
 
 # eof
