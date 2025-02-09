8f9d8eafe1f7186d074a5c30183f8582dddebecc
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-09 08:38:07 -0800

    First nsproto test in a while, passes.

--- rbc.console.mk
+++ rbc.console.mk
@@ -58,15 +58,13 @@ include $(zRBT_TEST_SRJCL)
 #  Test Targets
 #
 
-rbc-ta.%: rbs_define rbb_define rbn_define
-	$(MBC_START) "Run all tests"
-	$(MAKE) rbt_test_srjcl_bottle_service_rule   RBM_MONIKER=nsproto
-	$(MAKE) rbt_test_nsproto_bottle_service_rule RBM_MONIKER=srjcl
+rbc-ta.%: rbs_define rbb_define rbn_define \
+  rbt_test_nsproto_bottle_service_rule     \
+  rbt_test_srjcl_bottle_service_rule       \
+  # 
 	$(MBC_PASS)
 
-rbc-to.%: rbs_define rbb_define rbn_define
-	$(MBC_START) "Run tests for nameplate ->" $(RBM_MONIKER)
-	$(MAKE)  rbt_test_$(RBM_MONIKER)_bottle_service_rule
+rbc-to.%: rbs_define rbb_define rbn_define rbt_test_$(RBM_MONIKER)_bottle_service_rule
 	$(MBC_PASS)
 
 
