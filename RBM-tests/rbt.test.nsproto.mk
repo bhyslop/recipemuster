f8ba8d56a1deed081ecfb40b20ba9d3000074b3d
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-19 09:42:27 -0800

    Experiment to elevate all includes

--- RBM-tests/rbt.test.nsproto.mk
+++ RBM-tests/rbt.test.nsproto.mk
@@ -1,5 +1,18 @@
-# Configuration: where to find the Makefile Bash Console declarations
-include $(RBT_MBC_MAKEFILE)
+# Copyright 2024 Scale Invariant, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Brad Hyslop <bhyslop@scaleinvariant.org>
 
 # Main entry point
 rbt_test_bottle_service_rule:                \
