7a96f9cb6174802cb41dd66525d128c858e54029
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-02-27 14:11:03 -0800

    back with podman 5.2.1 seeing if I can unregress...

--- Tools/rbp.podman.mk
+++ Tools/rbp.podman.mk
@@ -127,7 +127,7 @@ rbp_start_service_rule: zrbp_validate_regimes_rule rbp_check_connection
 	$(MBC_STEP) "Creating BOTTLE container with namespace networking"
 	podman $(zRBP_CONN) run -d                                     \
 	  --name $(RBM_BOTTLE_CONTAINER)                               \
-	  --network ns:$(RBM_ENCLAVE_NS_DIR)/$(RBM_ENCLAVE_NAMESPACE)  \
+	  --network ns:/var/run/netns/$(RBM_ENCLAVE_NAMESPACE)         \
 	  --dns=$(RBRN_ENCLAVE_SENTRY_IP)                              \
 	  --cap-add net_raw                                            \
 	  --security-opt label=disable                                 \
