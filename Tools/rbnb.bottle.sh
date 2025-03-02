c505111126e513265e7995fe55ee13a283d99054
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-03-02 13:14:12 -0800

    Whoa more fix...

--- Tools/rbnb.bottle.sh
+++ Tools/rbnb.bottle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "RBNS: Beginning bottle namespace setup inside unshare context"
+echo "RBNS: Beginning bottle namespace setup script for unshare use"
 
 set -e
 set -x
@@ -14,25 +14,48 @@ set -x
 : ${RBRN_ENCLAVE_BOTTLE_IP:?}   && echo "RBNS0: RBRN_ENCLAVE_BOTTLE_IP   = ${RBRN_ENCLAVE_BOTTLE_IP}"
 : ${RBM_ENCLAVE_NAMESPACE:?}    && echo "RBNS0: RBM_ENCLAVE_NAMESPACE    = ${RBM_ENCLAVE_NAMESPACE}"
 
-echo "RBNS1: Creating network namespace"
+# Check if we're running as root in a user namespace
+if [ "$(id -u)" -eq 0 ]; then
+  echo "RBNS-NS: Running as root in user namespace, good."
+else
+  echo "RBNS-NS: ERROR - Not running as root in a user namespace."
+  echo "RBNS-NS: This script should be run inside 'unshare -r -n -m' context."
+  exit 1
+fi
+
+# Setup the network namespace
+echo "RBNS1: Setting up network namespace directory"
+mkdir -p /run/netns
+mount -t tmpfs netns /run/netns
+
+echo "RBNS2: Creating network namespace"
 ip netns add ${RBM_ENCLAVE_NAMESPACE}
 
-echo "RBNS2: Creating and configuring veth pair"
+echo "RBNS3: Creating and configuring veth pair"
 ip link add ${RBM_ENCLAVE_BOTTLE_OUT} type veth peer name ${RBM_ENCLAVE_BOTTLE_IN}
 
-echo "RBNS3: Moving veth endpoint to namespace"
+echo "RBNS4: Moving veth endpoint to namespace"
 ip link set ${RBM_ENCLAVE_BOTTLE_IN} netns ${RBM_ENCLAVE_NAMESPACE}
 
-echo "RBNS4: Configuring interface in namespace"
+echo "RBNS5: Configuring interface in namespace"
 ip netns exec ${RBM_ENCLAVE_NAMESPACE} ip link set ${RBM_ENCLAVE_BOTTLE_IN} name eth0
 ip netns exec ${RBM_ENCLAVE_NAMESPACE} ip addr add ${RBRN_ENCLAVE_BOTTLE_IP}/${RBRN_ENCLAVE_NETMASK} dev eth0
 ip netns exec ${RBM_ENCLAVE_NAMESPACE} ip link set eth0 up
 ip netns exec ${RBM_ENCLAVE_NAMESPACE} ip route add default via ${RBRN_ENCLAVE_SENTRY_IP} dev eth0
 ip netns exec ${RBM_ENCLAVE_NAMESPACE} ip link set lo up
 
-echo "RBNS5: Connecting namespace veth to bridge"
+echo "RBNS6: Connecting namespace veth to bridge"
 ip link set ${RBM_ENCLAVE_BOTTLE_OUT} master ${RBM_ENCLAVE_BRIDGE}
 ip link set ${RBM_ENCLAVE_BOTTLE_OUT} up
+
+echo "RBNS7: Verifying namespace exists"
+ls -la /run/netns/${RBM_ENCLAVE_NAMESPACE} || echo "WARNING: Namespace not found!"
+
+echo "RBNS8: Making namespace visible to host system"
+# We need to make the namespace created in our user namespace 
+# accessible to the host system for container attachment
+mkdir -p /var/run/netns
+mount --bind /run/netns/${RBM_ENCLAVE_NAMESPACE} /var/run/netns/${RBM_ENCLAVE_NAMESPACE}
 
 echo "RBNS: Bottle namespace setup complete"
 
