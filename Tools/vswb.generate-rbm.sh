26de055be21e8cf2d8369d0082e23872b1e0458b
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-04-19 13:18:45 -0700

    Okay, only wildcard needs quoting.

--- Tools/vswb.generate-rbm.sh
+++ Tools/vswb.generate-rbm.sh
@@ -5,11 +5,11 @@ set -e
 source "$(dirname "${BASH_SOURCE[0]}")/vswb.builder.sh"
 
 vswb_define_project    all           "Unifferentiated Files"
-vswb_add_files_recurse all ./ "*.sh" 
+vswb_add_files_recurse all ./ "*.sh"
 vswb_add_files_recurse all ./ "*.mk"
 
 vswb_init_workspace ALL vsusi all
 
-vswb_generate_files "../vswb_test" ".." "brm_recipemuster" "vsrbm"
+vswb_generate_files ../vswb_test .. brm_recipemuster vsrbm
 
 
