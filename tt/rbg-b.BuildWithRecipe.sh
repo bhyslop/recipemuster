df72b8d4931437419d994c1b5b3eae29b31a031c
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-07-03 07:00:39 -0700

    Cursor recommended fix to tabtarget.

--- tt/rbg-b.BuildWithRecipe.sh
+++ tt/rbg-b.BuildWithRecipe.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd "$(dirname "$0")/.." &&  Tools/mbd.dispatch.sh jp_single om_line "$(basename "$0")" \
-                              RBG_ARG_RECIPE="$1"       \
-                              RBG_ARG_FQIN_OUTPUT="$2"
+cd "$(dirname "$0")/.." &&\
+  RBG_ARG_RECIPE="$1"        \
+  RBG_ARG_FQIN_OUTPUT="$2"   \
+  Tools/mbd.dispatch.sh jp_single om_line "$(basename "$0")"
