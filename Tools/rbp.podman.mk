7629f8cb93b800c011e14469c16a3a41407b577a
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-07-03 08:49:20 -0700

    ip route setup fail.

--- Tools/rbp.podman.mk
+++ Tools/rbp.podman.mk
@@ -290,7 +290,9 @@ rbp_start_service_rule: zrbp_validate_regimes_rule rbp_check_connection
 	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "echo 'nameserver $(RBRN_ENCLAVE_SENTRY_IP)' > /etc/resolv.conf"
 
 	$(MBC_STEP) "Configure default route in CENSER"
-	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "ip route add default via $(RBRN_ENCLAVE_SENTRY_IP) || true"
+	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "ip route add default via $(RBRN_ENCLAVE_SENTRY_IP)"
+	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "ip route"
+	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "ip route | grep -q '^default via $(RBRN_ENCLAVE_SENTRY_IP)'"
 
 	$(MBC_STEP) "Flush any existing ARP entries and restart networking in CENSER"
 	podman $(RBM_CONNECTION) exec $(RBM_CENSER_CONTAINER) sh -c "ip link set eth0 down && ip link set eth0 up && ip -s -s neigh flush all"
