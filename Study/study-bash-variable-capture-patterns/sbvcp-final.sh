8f8a910d167d3a3bbd9bebcc1a299afd9003396c
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-07-21 06:51:51 -0700

    Final split declaration proof.

--- Study/study-bash-variable-capture-patterns/sbvcp-final.sh
+++ Study/study-bash-variable-capture-patterns/sbvcp-final.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# This is my 'smoking gun' to make sure that local var assignment and
+#   error propagation functions so that the exit code makes it out.
+#   This should always return exit code 23 with no output.
+
 failing_cmd() {
     echo "output_from_failing_cmd"
     return 1
