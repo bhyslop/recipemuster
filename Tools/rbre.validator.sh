639b490d3ee4e5ca28676da884832e83647f59e9
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-07-29 17:39:49 -0700

    Wrapping up, with a first and heavily untested ECR draft.

---
+++ Tools/rbre.validator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2025 Scale Invariant, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Brad Hyslop <bhyslop@scaleinvariant.org>
+
+set -e
+
+ZRBRE_SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "${ZRBRE_SCRIPT_DIR}/bvu_BashValidationUtility.sh"
+
+# RBRE-specific environment variables
+bvu_env_string      RBRE_AWS_CREDENTIALS_ENV     1    255
+bvu_env_string      RBRE_AWS_ACCESS_KEY_ID      20     20
+bvu_env_string      RBRE_AWS_SECRET_ACCESS_KEY  40     40
+bvu_env_string      RBRE_AWS_ACCOUNT_ID         12     12
+bvu_env_string      RBRE_AWS_REGION              1     32
+bvu_env_xname       RBRE_REPOSITORY_NAME         2     64
+
+# eof
+
