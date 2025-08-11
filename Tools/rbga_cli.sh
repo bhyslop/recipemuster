85fa8a2cfb826061159edcc249729296fb1d33c7
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-08-11 07:53:00 -0700

    Bug fix for cli.  ABOUT TO DEPLOY FIRST MAKEFILE-LESS DISPATCH...

--- Tools/rbga_cli.sh
+++ Tools/rbga_cli.sh
@@ -20,15 +20,15 @@
 
 set -euo pipefail
 
-ZRBMP_SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
+ZRBGA_SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
 
 # Source all dependencies
-source "${ZRBMP_SCRIPT_DIR}/bcu_BashCommandUtility.sh"
-source "${ZRBMP_SCRIPT_DIR}/bvu_BashValidationUtility.sh"
-source "${ZRBMP_SCRIPT_DIR}/rbl_Locator.sh"
-source "${ZRBMP_SCRIPT_DIR}/rbga_GoogleAdmin.sh"
+source "${ZRBGA_SCRIPT_DIR}/bcu_BashCommandUtility.sh"
+source "${ZRBGA_SCRIPT_DIR}/bvu_BashValidationUtility.sh"
+source "${ZRBGA_SCRIPT_DIR}/rbl_Locator.sh"
+source "${ZRBGA_SCRIPT_DIR}/rbga_GoogleAdmin.sh"
 
-zrbmp_furnish() {
+zrbga_furnish() {
   zrbl_kindle
   bvu_file_exists "${RBL_RBRR_FILE}"
   source "${RBL_RBRR_FILE}" || bcu_die "Failed to source RBRR regime file"
@@ -36,7 +36,7 @@ zrbmp_furnish() {
   zrbga_kindle
 }
 
-bcu_execute rbmp_ "Manual Setup Procedures" zrbmp_furnish "$@"
+bcu_execute rbga_ "Manual Setup Procedures" zrbga_furnish "$@"
 
 
 # eof
