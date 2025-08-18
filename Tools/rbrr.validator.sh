590fe66f993edd18cd7caee13423e058df046005
Brad Hyslop <bhyslop@scaleinvariant.org>
2025-08-18 10:56:38 -0700

    Fix machine type validation bug.

--- Tools/rbrr.validator.sh
+++ Tools/rbrr.validator.sh
@@ -49,8 +49,8 @@ bvu_env_gname       RBRR_GAR_REPOSITORY          1     63  # GAR repo id (loose 
 # Google Cloud Build Configuration
 bvu_env_gname       RBRR_GCB_PROJECT_ID          6     63  # Usually same as GAR project (loose gname check)
 bvu_env_gname       RBRR_GCB_REGION              1     32  # GCP build region (loose gname check; API enforces real rules)
-bvu_env_gname       RBRR_GCB_MACHINE_TYPE        1     64  # Machine type like e2-highcpu-8 (loose gname check)
-bvu_env_string      RBRR_GCB_TIMEOUT             2     10  # e.g., 1200s
+bvu_env_string      RBRR_GCB_MACHINE_TYPE        3     64  # Cloud Build machine type (API-native form, e.g., E2_HIGHCPU_8)
+bvu_env_string      RBRR_GCB_TIMEOUT             2     10  # Cloud Build timeout (seconds form, e.g., 600s, 1200s)
 
 # Service Account Configuration Files
 bvu_env_string      RBRR_ADMIN_RBRA_FILE         1    512  # Path to administrative service account env
