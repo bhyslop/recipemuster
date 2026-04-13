810f5a6b57a966db13c9337f0f54f40c73f52413
Brad Hyslop <bhyslop@scaleinvariant.org>
2026-04-13 15:13:40 -0700

    jjb:1011-af29ddb9:₢A6AAG:n: Remove CCCK launcher, tabtarget, and directory (retirement)

--- .buk/launcher.cccw_workbench.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Launcher stub - delegates to CCCK workbench
-source "${BASH_SOURCE[0]%/*}/../Tools/buk/bul_launcher.sh"
-bul_launch "${BURC_TOOLS_DIR}/ccck/cccw_workbench.sh" "$@"
--- tt/ccck-s.ConnectShell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export BURD_LAUNCHER=".buk/launcher.cccw_workbench.sh"
-export BURD_INTERACTIVE=1
-exec "${BASH_SOURCE[0]%/*}/../${BURD_LAUNCHER}" "${0##*/}" "${@}"
